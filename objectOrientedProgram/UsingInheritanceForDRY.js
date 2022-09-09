4da546c09d83449b73c61e06ad8944aa9132ba73
Fahim <fahimmakhanifm@gmail.com>
2022-09-09 11:21:30 +0530

    some more tried files

---
+++ objectOrientedProgram/UsingInheritanceForDRY.js
@@ -0,0 +1,18 @@
+//DRY Dont repert yourSelf
+function Cat(name) {
+    this.name = name;
+  }
+  
+  function Bear(name) {
+    this.name = name;
+  }
+  
+  
+  function Animal() { }
+  
+  Animal.prototype = {
+    constructor: Animal,
+    eat: function() {
+      console.log("nom nom nom");
+    }
+  };
