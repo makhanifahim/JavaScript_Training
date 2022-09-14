e75452ccc6fdfc367d46a2d780cfc8b096857708
Fahim <fahimmakhanifm@gmail.com>
2022-09-14 15:20:17 +0530

    Create FindAFemonaciSeries&OddCal.js

---
+++ SimplePrograms/FindAFemonaciSeries&OddCal.js
@@ -0,0 +1,15 @@
+function sumFibs(num) {
+    let prevNumber = 0;
+    let currNumber = 1;
+    let tot = 0;
+    while (currNumber <= num) {
+      if (currNumber % 2 !== 0) {
+        tot += currNumber;
+      }
+      currNumber += prevNumber;
+      prevNumber = currNumber - prevNumber;
+    }
+    return tot;
+  }
+  
+  sumFibs(4);
